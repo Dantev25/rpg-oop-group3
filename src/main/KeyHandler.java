a233e5da5953f897582f6d7c084708b53c516e2d
Mrishana <mrishanar2712@gmail.com>
2022-07-03 01:38:33 +0400

    Execute Menu Items

--- src/main/KeyHandler.java
+++ src/main/KeyHandler.java
@@ -32,9 +32,30 @@ public class KeyHandler implements KeyListener{
 			
 			if(code == KeyEvent.VK_W) {
 				gp.ui.commandNum--;
+				
+				if(gp.ui.commandNum < 0) {
+				gp.ui.commandNum = 2;
+				}
 			}
 			if(code == KeyEvent.VK_S) {
 				gp.ui.commandNum++;
+				
+				if(gp.ui.commandNum > 2) {
+				gp.ui.commandNum = 0;
+				}
+			}
+			if(code == KeyEvent.VK_ENTER) {
+				if(gp.ui.commandNum == 0) {
+					gp.gameState = gp.playState;
+					gp.playMusic(0);
+				}
+				if(gp.ui.commandNum == 1) {
+					//add later
+				}
+				if(gp.ui.commandNum == 2) {
+					System.exit(0);
+				}
+				
 			}
 		}
 		//PLAYSTATE
