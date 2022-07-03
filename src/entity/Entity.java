f77bd29792c44cf1792fd5be450910ac52002f9a
Mrishana <mrishanar2712@gmail.com>
2022-07-03 14:42:05 +0400

    Created an ArrayList and added all the entities in it.

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -16,7 +16,7 @@ public class Entity {
 	public int worldX, worldY;
 	public int speed;
 	public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
-	public String direction;
+	public String direction = "down";
 	public int spriteCounter = 0;
 	public int spriteNum = 1;
 	public Rectangle solidArea = new Rectangle(0, 0, 48, 48);
@@ -31,6 +31,10 @@ public class Entity {
 	
 	String dialogues[] = new String[20];
 	
+	
+	public BufferedImage image, image2, image3;
+	public String name;
+	public boolean collision = false;
 	
 	public Entity (GamePanel gp) {
 		this.gp = gp;
--- src/main/UI.java
+++ src/main/UI.java
@@ -6,8 +6,8 @@ import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
+import entity.Entity;
 import object.OBJ_Heart;
-import object.SuperObject;
 
 
 public class UI {
@@ -33,7 +33,7 @@ public class UI {
 		arial_80B = new Font("Arial", Font.BOLD, 80);
 		
 		//CREATE HUD OBJECT
-		SuperObject heart = new OBJ_Heart(gp);
+		Entity heart = new OBJ_Heart(gp);
 		heart_full = heart.image;
 		heart_half = heart.image2;
 		heart_blank = heart.image3;
