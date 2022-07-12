dc79462281490666c8b11327a1c1130a00687c7f
Sadiyahk25 <95752277+Sadiyahk25@users.noreply.github.com>
2022-07-12 16:50:32 +0400

    Added items. Tested program

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -57,7 +57,7 @@ public class GamePanel extends JPanel implements Runnable{
 	//ENTITY AND OBJECTS	
 	public Player player = new Player(this, keyH);
 	
-	public Entity obj[] = new Entity[10];
+	public Entity obj[] = new Entity[20];
 	
 	public Entity npc[] = new Entity[10];
 	
@@ -158,6 +158,7 @@ public class GamePanel extends JPanel implements Runnable{
 						monster[i].update();
 					}
 					if(monster[i].alive == false) {
+						monster[i].checkDrop();
 						monster[i] = null;
 					}
 				}
