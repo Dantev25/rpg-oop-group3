04390777f5460969de783e1cb2c45b0bedd43369
Sadiyahk25 <95752277+Sadiyahk25@users.noreply.github.com>
2022-07-13 15:00:35 +0400

    Added config file to store options settings. Tested the program

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -63,6 +63,8 @@ public class GamePanel extends JPanel implements Runnable{
 	
 	public EventHandler eHandler = new EventHandler(this);
 	
+	Config config = new Config(this);
+	
 	Thread gameThread;
 	
 	//ENTITY AND OBJECTS	
@@ -110,7 +112,9 @@ public class GamePanel extends JPanel implements Runnable{
 		tempScreen = new BufferedImage(screenWidth, screenHeight, BufferedImage.TYPE_INT_ARGB);
 		g2 = (Graphics2D)tempScreen.getGraphics();
 		
-		setFullScreen();
+		if(fullScreenOn == true) {
+			setFullScreen();
+		}
 		
 	}
 	
--- src/main/UI.java
+++ src/main/UI.java
@@ -597,6 +597,8 @@ public class UI {
 		g2.drawRect(textX, textY, 120, 24);
 		volumeWidth = 24 * gp.se.volumeScale;
 		g2.fillRect(textX, textY, volumeWidth, 24);
+		
+		gp.config.saveConfig();
 	}
 	
 	public void options_fullscreenNotification(int frameX, int frameY) {
